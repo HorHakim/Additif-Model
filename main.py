fef9007b0f656a0c91ecbf24f48ad877eab98cd9
Hakim Horairy <horairyhakim@gmail.com>
2024-01-16 12:52:32 +0100

    bug correction : variable name

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from data_preparation import DataPreparation
-from regression import Regression
+from additif import Additif
 
 
 csv_path = "./vente_maillots_de_bain.csv"
 data_preparation_object = DataPreparation(csv_path)
-regression_object = Regression(data_preparation_object)
+additif_object = Additif(data_preparation_object)
 
 # data_preparation_object.show_graph()
